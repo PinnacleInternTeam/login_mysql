0fdf82e74da8f5dcd3cb631ca2d42423a3547e44
richa <devteamri@gmail.com>
2023-02-11 13:52:17 +0530

    mysql

---
+++ backend/Server.js
@@ -0,0 +1,37 @@
+const express=require("express");
+const mysql=require("mysql");
+const cors=require("cors");
+
+const app=express();
+app.use(express.json());
+app.use(cors());
+
+const con=mysql.createConnection({
+    user:"root",
+    host:"localhost",
+    password:"",
+    database:"register"
+})
+app.post('/register', (req, res) => {
+    const email = req.body.email;
+    const username = req.body.username;
+    const password = req.body.password;
+
+    con.query("INSERT INTO users (email, username, password) VALUES (?, ?, ?)", [email, username, password], 
+        (err, result) => {
+            if(result){
+                res.send(result);
+            }else{
+                res.send({message: "ENTER CORRECT ASKED DETAILS!"})
+            }
+        }
+    )
+})
+
+app.listen(5000, () => {
+    console.log("running backend server");
+})
+        
+
+
+
---
+++ src/Components/Register.js
@@ -0,0 +1,49 @@
+import React,{useState} from 'react'
+import Axios from "axios"
+
+export default function Register() {
+    const [email, setEmail] = useState("");
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+
+  const [registerStatus, setRegisterStatus] = useState("");
+
+  const register = (e) => {
+    e.preventDefault();
+    Axios.post("http://localhost:5000/register", {
+      email: email,
+      username: username,
+      password: password,
+    }).then((response) => {
+      // setRegisterStatus(response);
+      // console.log(response);
+      if(response.data.message){
+        setRegisterStatus(response.data.message);
+      }else{
+        setRegisterStatus("ACCOUNT CREATED SUCCESSFULLY");
+      }
+    })
+  }
+
+  return (
+    <div>
+    <div className='container' style={{background:"#CBD6E2"}}>
+         <div className="loginForm text-center">
+        <form >
+          <h4>Register Here</h4>
+          <label htmlFor="email">Email Address*</label><br/>
+          <input className="textInput" type="text" name="email" onChange={(e) => {setEmail(e.target.value)}} placeholder="Enter your Email Address" required /><br/><br/>
+          <label htmlFor="username">Username*</label><br/>
+          <input className="textInput" type="username" name="username" onChange={(e) => {setUsername(e.target.value)}} placeholder="Enter your Username" required /><br/><br/>
+          <label htmlFor="password">Password*</label><br/>
+          <input className="textInput" type="password" name="password" onChange={(e) => {setPassword(e.target.value)}} placeholder="Enter your Password" required /><br/><br/>
+          <input className="btn btn-outline-success" type="submit" onClick={register} value="Create an account" /><br/>
+          <h1 style={{fontSize: '15px', textAlign: 'center', marginTop: '20px'}}>{registerStatus}</h1><br/><br/>
+        </form>
+      </div>
+    </div>
+    </div>
+      
+   
+  )
+}
